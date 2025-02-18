2658611a085d88a05abe4b1c045e4d4c5e65999b
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-17 17:14:54 -0800

    Time: 0 ms (100%), Space: 23.9 MB (50.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -6,19 +6,14 @@ private:
             b = temp;
     }
 public:
- void moveZeroes(vector<int>& nums) {
-        int lastNonZeroFoundAt = 0; // Pointer for the position of the next non-zero element
-
-        // Move all non-zero elements to the beginning of the array
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[lastNonZeroFoundAt++] = nums[i];
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int Last_Non_Zero_Element_Idx=0;
+        for(int i=0 ; i <n ; i++){
+            if(nums[i]!=0){
+                Swap_Nums(nums[i],nums[Last_Non_Zero_Element_Idx]);
+                Last_Non_Zero_Element_Idx++;
             }
-        }
-
-        // Fill the rest of the array with zeroes
-        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
-            nums[i] = 0;
         }
     }
 };
