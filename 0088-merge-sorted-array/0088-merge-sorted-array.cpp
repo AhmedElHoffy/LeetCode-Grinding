0cde338fd637ca59b1086c8136ffee324278450e
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-22 09:28:51 -0800

    Time: 0 ms (100%), Space: 12.3 MB (70.61%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int Ptr1=m-1,Ptr2=n-1;
+        int Ptr3 = m+n-1;
+        while(Ptr1>= 0 && Ptr2 >=0 ){
+            if(nums1[Ptr1] > nums2[Ptr2]){
+                nums1[Ptr3] = nums1[Ptr1];
+                Ptr1--;
+            }else{
+                nums1[Ptr3] = nums2[Ptr2];
+                Ptr2--;
+            }
+            Ptr3--;
+        }
+        while(Ptr2>=0){
+            nums1[Ptr3]=nums2[Ptr2];
+            Ptr3--;
+            Ptr2--;
+        }
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int ptr_m=m-1;
+        int ptr_n = n-1;
+
+        int ptr_mn = m+n-1;
+        while( ptr_m>=0 && ptr_n>=0){
+            if(nums1[ptr_m] > nums2[ptr_n]){
+                nums1[ptr_mn--] = nums1[ptr_m--];
+            }else{
+                nums1[ptr_mn--] = nums2[ptr_n--];
+            }
+        }
+        while(ptr_n >=0){
+            nums1[ptr_mn--]=nums2[ptr_n--];
+        }
+    }
+};
+
+*/
