bc64158eb719806daefebf1b3e222aeaf2fb706b
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-22 08:11:33 -0800

    Time: 33 ms (93.77%), Space: 45.5 MB (44.62%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        int Curr_Sum=0;
+        int counter=0;
+        int temp;
+        unordered_map<int,int>Prefix_SumFreq;
+        Prefix_SumFreq[0]++;
+
+        for(int num : nums){
+            Curr_Sum+=num;
+            temp = Curr_Sum - k;
+            if(Prefix_SumFreq.find(temp)!=Prefix_SumFreq.end()){
+                counter+=Prefix_SumFreq[temp];
+            }
+                Prefix_SumFreq[Curr_Sum]++;
+        }
+        return counter;
+    }
+};
+
+//Prefix_Sum = [1,2,3] , k = 2; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
         // Sliding window will work for +ve values not for -ve Values
         // Since the range of values are -ve to +ve (Hash Table is our best option rather than using 
         // Sliding Window)
@@ -22,3 +60,6 @@ public:
         return counter;
   }
 };
+
+
+*/
