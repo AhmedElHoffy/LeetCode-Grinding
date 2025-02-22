1a60bb90f348037f7d8a384c4dd1b077c82d2218
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-21 21:33:24 -0800

    Time: 2 ms (39.71%), Space: 9.8 MB (79.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -19,30 +19,24 @@ private:
         }
     }
 public:
-   bool isPalindrome(string s) {
-        int left = 0, right = s.length() - 1;
+    bool isPalindrome(string Str) {
+        int n = Str.length();
+        int Left=0,Right = n-1;
 
-        while (left < right) {
-            // Move left pointer to the next alphanumeric character
-            while (left < right && !isalnum(s[left])) {
-                left++;
+        while(Left < Right){
+            while(Left < Right && !Char_Detect(Str[Left])){
+                Left++;
             }
-            // Move right pointer to the previous alphanumeric character
-            while (left < right && !isalnum(s[right])) {
-                right--;
+            while(Left < Right && !Char_Detect(Str[Right])){
+                Right--;
             }
-
-            // Compare characters (case-insensitive)
-            if (tolower(s[left]) != tolower(s[right])) {
+            if(Covnert_Upper2Lower(Str[Left])!= Covnert_Upper2Lower(Str[Right])){
                 return false;
             }
-
-            // Move pointers inward
-            left++;
-            right--;
+            Left++;
+            Right--;
         }
-
-        return true; // If no mismatches are found
+        return true;
     }
 };
 
