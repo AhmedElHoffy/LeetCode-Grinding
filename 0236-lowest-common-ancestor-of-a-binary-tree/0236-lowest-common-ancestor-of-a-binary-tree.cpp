ae5024b8cb8b460881bb4c38fd5a72497d984135
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-22 17:27:48 -0800

    Time: 12 ms (63.73%), Space: 17.5 MB (41.21%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+   
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // return null ptr if neither p or q are not found  or return Curr_Node if either was found
+        TreeNode* Curr_Node = root;
+        if(!Curr_Node || Curr_Node==p || Curr_Node==q){
+            return Curr_Node;
+        }
+        // DFS on both sides of the tree Left Side and Right side respectively (In a recursive approach)
+        TreeNode* LeftChild = lowestCommonAncestor(Curr_Node->left,p,q);
+        TreeNode* RightChild = lowestCommonAncestor(Curr_Node->right,p,q);
+
+        // If both Traversal returns Nodes; therefore, the Curr_Node is the LCA for p and q
+        if(LeftChild && RightChild){
+            return Curr_Node;
+        }
+
+        // Otherwise return the non-null Recursive approach  
+        //return the non-null child (either LeftChild or RightChild)
+        return (LeftChild)? LeftChild : RightChild;
+
+    }
+};
+
+
+
+/*
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+       // Base Case
+        if (!root || root == p || root == q) {
+            return root;
+        }
+
+        // Recursive calls for left and right subtrees
+        TreeNode* LeftChild = lowestCommonAncestor(root->left, p, q);
+        TreeNode* RightChild = lowestCommonAncestor(root->right, p, q);
+
+        // If both left and right return non-null, root is the LCA
+        if (LeftChild && RightChild) {
+            return root;
+        }
+
+        // Otherwise, return the non-null child (either LeftChild or RightChild)
+        return LeftChild ? LeftChild : RightChild;
+        
+    }
+};
+*/
