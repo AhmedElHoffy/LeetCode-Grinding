0f5b8f58eb463b8fcea86c216f4960fdc51e4850
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-22 17:29:22 -0800

    Time: 12 ms (63.73%), Space: 17.3 MB (67.55%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,17 +12,17 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         // return null ptr if neither p or q are not found  or return Curr_Node if either was found
-        TreeNode* Curr_Node = root;
-        if(!Curr_Node || Curr_Node==p || Curr_Node==q){
-            return Curr_Node;
+        //TreeNode* Curr_Node = root;
+        if(!root || root==p || root==q){
+            return root;
         }
         // DFS on both sides of the tree Left Side and Right side respectively (In a recursive approach)
-        TreeNode* LeftChild = lowestCommonAncestor(Curr_Node->left,p,q);
-        TreeNode* RightChild = lowestCommonAncestor(Curr_Node->right,p,q);
+        TreeNode* LeftChild = lowestCommonAncestor(root->left,p,q);
+        TreeNode* RightChild = lowestCommonAncestor(root->right,p,q);
 
         // If both Traversal returns Nodes; therefore, the Curr_Node is the LCA for p and q
         if(LeftChild && RightChild){
-            return Curr_Node;
+            return root;
         }else if(LeftChild){          
         // Otherwise return the non-null Recursive approach  
         //return the non-null child (either LeftChild or RightChild)
