00d17ee6668800a09b726d1f590d5ad7a97221d1
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-02-23 14:10:34 -0800

    Time: 3 ms (91.35%), Space: 15.1 MB (49.03%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -30,7 +30,7 @@ public:
 
         Curr = head;
         while(Curr!=nullptr){
-            Node* temp = Old_to_Copy_Map[Curr];
+            //Node* temp = Old_to_Copy_Map[Curr];
             Old_to_Copy_Map[Curr]->next = Old_to_Copy_Map[Curr->next] ;
             Old_to_Copy_Map[Curr]->random = Old_to_Copy_Map[Curr->random] ;
             //temp->next   = (Curr->next)   ?  Old_to_Copy_Map[Curr->next] : nullptr;
