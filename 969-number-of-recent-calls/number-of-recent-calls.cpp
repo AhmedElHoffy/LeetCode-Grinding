e2e08ae6f9ff5692f9968b924afc1945f59cca49
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-03-16 23:47:00 -0700

    Time: 40 ms (11.21%) | Memory: 64.2 MB (40.08%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -0,0 +1,51 @@
+class RecentCounter {
+public:
+ queue<int> Q_Calls;        
+    RecentCounter() {
+        // Constructor initializes the queue (no additional logic needed)
+    }
+    
+    int ping(int t) {
+
+        Q_Calls.push(t);
+
+        while( t - Q_Calls.front() > 3000){
+            Q_Calls.pop();
+        }
+        return Q_Calls.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+
+
+
+
+ /*
+class RecentCounter {
+public:
+    queue<int> Result;
+    RecentCounter() {
+    // Constructor initializes the queue (no additional logic needed)
+    }
+    
+    int ping(int t) {
+        Result.push(t);
+        while(Result.front() < t-3000){
+            Result.pop();
+        }
+        return Result.size();
+    }
+};
+*/
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+
+ 
