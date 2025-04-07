76a2ca95a0a94aa955a3a5af416337244d7c9089
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-06 21:47:26 -0700

    Time: 0 ms (100.00%) | Memory: 113.8 MB (44.96%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,82 @@
+class Solution {
+
+public:
+    double findMaxAverage(vector<int>& Nums, int k) {
+      int n = Nums.size();
+      double Max_Avg=INT_MIN;
+      double Curr_Sum;
+      for(int i=0 ; i<k ; i++){
+        Curr_Sum+=Nums[i];
+      }
+      Max_Avg = Curr_Sum/k;
+      
+      for(int j=k; j<n ; j++){
+        Curr_Sum+=Nums[j];
+        Curr_Sum-=Nums[j-k];
+        if((Curr_Sum/k) > Max_Avg){
+            Max_Avg= Curr_Sum/k;
+        }
+      }
+      return Max_Avg;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    int Max_num(int x, int y){
+        if(x>= y ){
+            return x;
+        }else{
+            return y;
+        }
+    }
+    double findMaxAverage(vector<int>& nums, int k) {
+        double Max_Sum=0;
+        double Curr_Sum=0;
+
+        // Get the first window sum
+        for(int i=0; i < k ; i++){
+            Curr_Sum+=nums[i];
+        }
+        Max_Sum = Curr_Sum;
+
+        // Moving the Sliding Window one by one
+        for(int j=k ; j<nums.size() ; j++ ){
+            Curr_Sum = Curr_Sum + nums[j] - nums[j-k];
+            Max_Sum = Max_num(Curr_Sum,Max_Sum);
+        }
+        return Max_Sum / k;
+    }
+};
+*/
