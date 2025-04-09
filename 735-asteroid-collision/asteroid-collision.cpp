7be950ca1168f80740ef6d3ec242660a83a82d85
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-08 21:34:17 -0700

    Time: 1 ms (42.81%) | Memory: 22.1 MB (41.58%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,35 +1,39 @@
 class Solution {
+        // Cases:
+        //  1. Curr(+ve) && New(+ve) (Good)
+        //  2. Curr(-ve) && New(-ve) (Good)
+        //  3. Curr(-ve) && New(+ve) (Good)
+        //  4. Curr(+ve) && New(-ve) (Collision)
+        //      a. Curr >  abs(New) Keep Curr and Destroy New 
+        //      b. Curr == abs(New) Destroy Both
+        //      c. Curr <  abs(New) Destroy Curr and Add New
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> Stk;
+    vector<int> asteroidCollision(vector<int>& Asteroids) {
+        stack<int>Stk;
 
-        for (int asteroid : asteroids) {
-            bool destroyed = false;
-
-            while (!Stk.empty() && Stk.top() > 0 && asteroid < 0) {
-                if (Stk.top() + asteroid == 0) {
+        for(int Ast : Asteroids){
+            if(Ast > 0){
+                Stk.push(Ast);
+            }else{
+                while(!Stk.empty() && Stk.top()>0 &&  Stk.top()<-Ast){
                     Stk.pop();
-                    destroyed = true;
-                    break;
-                } else if (abs(Stk.top()) > abs(asteroid)) {
-                    destroyed = true;
-                    break;
-                } else {
+                }
+
+                if(Stk.empty() || Stk.top()<0){
+                    Stk.push(Ast);
+                }
+
+                if(!Stk.empty() &&  Stk.top() == -Ast){
                     Stk.pop();
                 }
             }
-
-            if (!destroyed) {
-                Stk.push(asteroid);
-            }
         }
-
-        vector<int> Result;
-        while (!Stk.empty()) {
-            Result.push_back(Stk.top());
+        vector<int>Result(Stk.size());
+        int j = Result.size()-1;
+        while(!Stk.empty()){
+            Result[j--] = Stk.top();
             Stk.pop();
         }
-        reverse(Result.begin(), Result.end());
         return Result;
     }
 };
