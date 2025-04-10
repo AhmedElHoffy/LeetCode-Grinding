fb5e7738720e3c382b844b88433f3796417821b4
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-09 23:40:05 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (75.67%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -13,8 +13,34 @@ class Solution {
 public:
     
     int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        return 1+ max(maxDepth(root->left),maxDepth(root->right));
-        
+        if(!root){
+            return 0;
+        }
+        int Left_Depth=maxDepth(root->left);
+        int Right_Depth=maxDepth(root->right);
+
+        return max(Left_Depth,Right_Depth) + 1;
     }
 };
+
+
+
+/*
+ stack<pair<TreeNode*,int>>Stk;
+        int Height=1;
+        if(!root) return 0;
+
+        Stk.push({root,Height});
+        int Max_Depth=0;
+        while(!Stk.empty()){
+            auto [Curr_Node,Curr_Depth] = Stk.top();
+            Stk.pop();
+            
+            Max_Depth = max(Max_Depth,Curr_Depth);
+
+            if(Curr_Node->left) Stk.push({Curr_Node->left,Curr_Depth+1});
+            if(Curr_Node->right) Stk.push({Curr_Node->right,Curr_Depth+1});
+        }
+        return Max_Depth;
+    }
+*/
