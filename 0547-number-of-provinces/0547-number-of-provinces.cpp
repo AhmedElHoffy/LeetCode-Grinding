da09185d97ac225938ddcf3bbd7f6860e030fd32
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-11 19:05:57 -0700

    Time: 0 ms (100%), Space: 19.7 MB (59.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,30 @@
 class Solution {
-private:
-    void DFS_Helper(vector<vector<int>>& isConnected, 
-                   unordered_set<int>&Visited_Cities,int i){
-        int n=isConnected.size();
-        Visited_Cities.insert(i);
-        for(int j=0; j<n ;j++){
-            if(isConnected[i][j] && (Visited_Cities.count(j)==0)){
-                DFS_Helper(isConnected,Visited_Cities,j);
-            }
-        }
-        return;
-    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
+        int n = isConnected.size();
+        vector<bool>Visitied_Cities(n,false);
         int Provinces_Num=0;
-        unordered_set<int> Visited_Cities;
-        for(int i=0 ; i<n ; i++){
-            {
-                for(int j=0; j<n ; j++){
-                    if(Visited_Cities.count(i)==0){
-                        Provinces_Num++;
-                        DFS_Helper(isConnected,Visited_Cities,i);
+        stack<int>Stk;
+        int Curr_City;
+        for(int i=0; i<n ; i++){
+            if(!Visitied_Cities[i]){
+                Stk.push(i);
+            
+            while(!Stk.empty()){
+                Curr_City = Stk.top();
+                Stk.pop();
+
+                if(!Visitied_Cities[Curr_City]){
+                    Visitied_Cities[Curr_City] = true;
+
+                    for(int j=0; j<n ; j++){
+                        if(isConnected[Curr_City][j] && !Visitied_Cities[j]){
+                            Stk.push(j);
+                        }
                     }
                 }
+            }
+            Provinces_Num++;
             }
         }
         return Provinces_Num;
