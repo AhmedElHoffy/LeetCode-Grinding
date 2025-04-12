f6ecd77e29c6e7c7c2e6e01122d6fae315db5592
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-12 12:38:32 -0700

    Time: 3 ms (20.22%) | Memory: 12.1 MB (40.41%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
-    double DFS_Helper(string Curr_Node, string Target_Node, unordered_map<string,vector<pair<string,double>>>Graph,
-                    unordered_set<string>&Visited_Nodes){
-                        if(Curr_Node == Target_Node){
-                            return 1.0;
-                        }
-                        Visited_Nodes.insert(Curr_Node);
-
-                        for(auto& [Nei , Wei] : Graph[Curr_Node]){
-                            if(!Visited_Nodes.count(Nei)){
-                                double Curr_Prod = DFS_Helper(Nei,Target_Node,Graph,Visited_Nodes);
-                                if(Curr_Prod!=-1.0) {
-                                    return Curr_Prod * Wei;
-                                }
-                            }
-                        }
-                        return -1.0;
-                    }
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+
         int n = equations.size();
         unordered_map<string,vector<pair<string,double>>>Graph;
         string Numerator,Denomerator;
@@ -30,17 +14,47 @@ public:
             Graph[Numerator].push_back({Denomerator,Val});
             Graph[Denomerator].push_back({Numerator,1.0/Val});
         }
+
         vector<double>Result;
-        string Start_Node, Last_Node;
+        string Start_Node, Target_Node;
         for(auto& q : queries){
-            Start_Node=q[0] , Last_Node=q[1];
-            if(Graph.find(Start_Node)==Graph.end() || Graph.find(Last_Node)==Graph.end()){
+            Start_Node=q[0] , Target_Node=q[1];
+            if(Graph.find(Start_Node)==Graph.end() || Graph.find(Target_Node)==Graph.end()){
                 Result.push_back(-1.0);
                 continue;
             }
+            queue<pair<string,double>>Q;
+            Q.push({Start_Node,1.0});
             unordered_set<string>Visited_Nodes;
-            Result.push_back(DFS_Helper(Start_Node,Last_Node,Graph,Visited_Nodes));
+            Visited_Nodes.insert(Start_Node);
+            bool Reached_Target = false;
+
+            // BFS Workflow
+
+            while(!Q.empty()){
+                auto [Curr_Node ,  Curr_Cost] = Q.front();
+                Q.pop();
+
+                if(Curr_Node == Target_Node){
+                    Result.push_back(Curr_Cost);
+                    Reached_Target = true;
+                    break;
+                }
+
+                for(auto& [Nei , Wei] : Graph[Curr_Node]){
+                    if(!Visited_Nodes.count(Nei)){
+                        Visited_Nodes.insert(Nei);
+                        Q.push({Nei, Curr_Cost*Wei});
+                    }
+                }
+
+            }  // End Clause for While Loop (BFS_Q)
+        
+        if(!Reached_Target){
+            Result.push_back(-1.0);
         }
+
+        }  // End Clause for For Loop (queries Traverse)
         return Result;
     }
 };
