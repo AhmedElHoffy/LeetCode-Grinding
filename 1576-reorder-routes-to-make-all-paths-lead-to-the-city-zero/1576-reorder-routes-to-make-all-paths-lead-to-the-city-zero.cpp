e077891c5d198296365415a7f6f872a2024a3cee
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-11 22:09:09 -0700

    Time: 87 ms (86.37%), Space: 104.9 MB (90.19%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,39 +1,38 @@
 class Solution {
-private: 
-    void DFS_Helper(int Curr_Node,vector<vector<int>> connections, vector<vector<pair<int,string>>>Graph, vector<bool>&Visited_Nodes, int &Changes){
-        Visited_Nodes[Curr_Node]=true;
 
-        for(auto &[Neighbor,Direc] : Graph[Curr_Node]){
-            if(!Visited_Nodes[Neighbor]){
-                if(Direc=="OutWard")Changes++;
-                DFS_Helper(Neighbor,connections,Graph,Visited_Nodes,Changes);
+public:
+    
+int minReorder(int n, vector<vector<int>>& connections) {
+    vector<vector<pair<int,int>>>Graph(n);
+
+    for(auto& Conn : connections){
+        Graph[Conn[0]].push_back({Conn[1],1});
+        Graph[Conn[1]].push_back({Conn[0],0});
+    }
+
+    vector<bool>Visited(n,false);
+
+    queue<int>Q_BFS;
+
+    Q_BFS.push(0);
+    Visited[0]=true;
+    int Changes=0;
+    int Curr_Node;
+    while(!Q_BFS.empty()){
+        Curr_Node=Q_BFS.front();
+        Q_BFS.pop();
+
+        for(auto& [Neighbor,ChangeNeed] : Graph[Curr_Node]){
+            if(!Visited[Neighbor]){
+                Changes+=ChangeNeed;
+                Visited[Neighbor]=true;
+                Q_BFS.push(Neighbor);
             }
         }
     }
-public:
-    void dfs(int node, vector<vector<pair<int, int>>>& graph, vector<bool>& visited, int& changes) {
-    visited[node] = true;
-
-    for (auto& [neighbor, needsChange] : graph[node]) {
-        if (!visited[neighbor]) {
-            changes += needsChange;
-            dfs(neighbor, graph, visited, changes);
-        }
-    }
+      return Changes;
 }
 
-int minReorder(int n, vector<vector<int>>& connections) {
-    vector<vector<pair<int, int>>> graph(n);
-    for (auto& conn : connections) {
-        graph[conn[0]].push_back({conn[1], 1});
-        graph[conn[1]].push_back({conn[0], 0});
-    }
-
-    vector<bool> visited(n, false);
-    int changes = 0;
-    dfs(0, graph, visited, changes);
-    return changes;
-}
 };
 
 
@@ -71,4 +70,34 @@ int minReorder(int n, vector<vector<int>>& connections) {
         return Changes_Needed;
     }
 
+*/
+
+
+
+// DFS Recursive Approach ;  Time: O(N) ,  Space: O(N)
+
+/*
+void dfs(int node, vector<vector<pair<int, int>>>& graph, vector<bool>& visited, int& changes) {
+    visited[node] = true;
+
+    for (auto& [neighbor, needsChange] : graph[node]) {
+        if (!visited[neighbor]) {
+            changes += needsChange;
+            dfs(neighbor, graph, visited, changes);
+        }
+    }
+}
+
+int minReorder(int n, vector<vector<int>>& connections) {
+    vector<vector<pair<int, int>>> graph(n);
+    for (auto& conn : connections) {
+        graph[conn[0]].push_back({conn[1], 1});
+        graph[conn[1]].push_back({conn[0], 0});
+    }
+
+    vector<bool> visited(n, false);
+    int changes = 0;
+    dfs(0, graph, visited, changes);
+    return changes;
+}
 */
