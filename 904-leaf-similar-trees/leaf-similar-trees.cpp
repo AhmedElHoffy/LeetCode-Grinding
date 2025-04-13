93fb7ff2666dd588f727c1927ebb0f3841faa213
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-12 22:40:40 -0700

    Time: 0 ms (100.00%) | Memory: 15.2 MB (86.10%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -11,7 +11,35 @@
  */
 class Solution {
 public:
+    int GetNextLeaf(stack<TreeNode*> &Stk){
+        TreeNode* Curr_Node;
+        while(!Stk.empty()){
+            Curr_Node = Stk.top();
+            Stk.pop();
+
+            if(Curr_Node->left) Stk.push(Curr_Node->left);
+            if(Curr_Node->right) Stk.push(Curr_Node->right);
+            if(!Curr_Node->left && !Curr_Node->right) return Curr_Node->val;
+        }
+        return -1;
+    }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        stack<TreeNode*>Stk1,Stk2;
+        Stk1.push(root1),Stk2.push(root2);
+        int Leaf1,Leaf2;
+        while(!Stk1.empty() && !Stk2.empty()){
+            Leaf1 = GetNextLeaf(Stk1);
+            Leaf2 = GetNextLeaf(Stk2);
+            if(Leaf1!=Leaf2) return false;
+        }
+        return Stk1.empty() && Stk2.empty();
+    }
+};
+
+
+
+/*
+bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         stack<TreeNode*>Stk_Tree1;
         stack<TreeNode*>Stk_Tree2;
 
@@ -51,4 +79,4 @@ public:
         cout<<"]"<<endl;
         return ( (Leaves_Tree1 == Leaves_Tree2) ? true:false);
     }
-};
+*/
