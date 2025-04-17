8b5d49b9a2c088770be72ef6d02d046b03302163
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-16 23:01:58 -0700

    Time: 0 ms (100%), Space: 8.7 MB (44.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,47 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-         if (n == 0) return 1;  // Base case: x^0 = 1
+    double myPow(double &x, int n) {
+        long long Power = n;
+        if(Power<0){
+            x = 1 /x;
+            Power = -Power;
+        }
+        if(Power==0) return 1;
+
+        double Half = myPow(x,Power/2);
+        if(Power%2==0){
+            return Half * Half;
+        }else{
+            return Half * Half* x;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ double myPow(double x, int n) {
+    if (n == 0) return 1;  // Base case: x^0 = 1
         if (x == 0) return 0;  // Base case: 0^n = 0 (for n > 0)
 
         // Handle negative n by converting to long long to avoid overflow
@@ -16,5 +56,5 @@ public:
 
         // If n is negative, return 1 / result
         return (n < 0) ? 1 / result : result;
-    }
-};
+        }
+*/
