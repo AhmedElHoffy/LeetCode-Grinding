d65665b6dd1f6dd857e3957d9dde6a9c41f7db3a
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-04-16 21:35:44 -0700

    Time: 4 ms (43.61%), Space: 73.8 MB (17.15%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.cpp
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
-    vector<int> findBuildings(vector<int>& heights) {
+    vector<int> findBuildings(vector<int>& Heights) {
+     int n = Heights.size();
+     stack<int>Stk;
+     int Max_Height=INT_MIN;
+
+     for(int j=n-1 ; j>=0 ; j--){
+        if(Heights[j]>Max_Height){
+            Max_Height=Heights[j];
+            Stk.push(j);
+        }
+     }
+
+     vector<int>Result;
+     while(!Stk.empty()){
+        Result.push_back(Stk.top());
+        Stk.pop();
+     } 
+     return Result;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ vector<int> findBuildings(vector<int>& heights) {
         int n = heights.size();
         int Max_Height=INT_MIN;
         stack<int>Stk;
@@ -17,16 +55,7 @@ public:
         }
         return Result;
     }
-};
-
-
-
-
-
-
-
-
-
+*/
 
 
 
