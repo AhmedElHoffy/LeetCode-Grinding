5d4c60ab0d6537af10c7991669933a732a536915
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-11-10 01:04:34 -0800

    Time: 0 ms (100.00%) | Memory: 10.1 MB (93.81%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,102 +1,30 @@
 class Solution {
-private:
-
-    bool Vowel_Detect_Fn(char ch){
-        if(ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e'||
-           ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o' || 
-           ch == 'U' || ch == 'u'){
+public:
+    bool isVowel(char ch){
+        if(ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I'
+           || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U'){
             return true;
            }
            return false;
     }
 
-    void Swap_Chars(char &a, char &b){
+    void Swap_Ch(char &a , char &b){
         char temp = a;
-        a = b;
+        a =b;
         b = temp;
     }
+    string reverseVowels(string &str) {
+        int n = str.size();
+        int i=0 , j = n-1;
 
-public:
-    string reverseVowels(string &Str) {
-       int n = Str.size();
-
-       int Left_Ptr = 0, Right_Ptr=n-1;
-
-       while(Left_Ptr < Right_Ptr){
-
-         while(Left_Ptr < Right_Ptr  && !Vowel_Detect_Fn(Str[Left_Ptr])){
-            Left_Ptr++;
-         }
-
-        while(Left_Ptr < Right_Ptr  && !Vowel_Detect_Fn(Str[Right_Ptr])){
-            Right_Ptr--;
-         }
-
-         if(Left_Ptr < Right_Ptr){
-            Swap_Chars(Str[Left_Ptr] , Str[Right_Ptr]);
-            Left_Ptr++;
-            Right_Ptr--;
-         }
-
-       }
-
-       return Str;
+        while( i< j){
+            while (i < j && !isVowel(str[i])) ++i;
+            while (i < j && !isVowel(str[j])) --j;
+            if (i < j) {                  // re-check before touching str[i], str[j]
+                Swap_Ch(str[i], str[j]);   // or std::swap(str[i], str[j])
+                ++i; --j;
+            }
+        }
+        return str;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*    bool Vowel_Letter(char c){
-        if(c =='a' ||c =='A' || c=='E' || c=='e' || c=='O' || c=='o' || c=='U' || c=='u' || c=='i' || c=='I'){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    void swap_char(char &c1, char &c2){
-        char temp = c1;
-        c1 = c2;
-        c2 = temp;
-    }
-    string reverseVowels(string s) {
-        int size_str = s.size();
-        int left = 0;
-        int right = size_str-1;
-        string Result = s;
-        while(left < right){
-            if(Vowel_Letter(Result[left])){
-                while(!Vowel_Letter(Result[right])){
-                    right--;
-                }
-                swap_char(Result[left],Result[right]);
-                right--;
-            }
-            left++;
-        }
-
-       return Result;
-    }
-*/
