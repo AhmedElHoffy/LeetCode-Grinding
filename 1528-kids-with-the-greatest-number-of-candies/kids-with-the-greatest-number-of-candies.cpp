32efad979b1780bb16f109f1bb95a2b8fe5478f6
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-11-10 00:18:21 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (24.38%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,83 +1,18 @@
 class Solution {
 public:
-    vector<bool> kidsWithCandies(vector<int>& Candies, int ExtraCandies) {
-        int Max_Candies = INT_MIN;
-        int n = Candies.size();
-        vector<bool> Result(n,false);
-        
-        for(int Candy:Candies){
-            if(Candy>=Max_Candies){
-                Max_Candies = Candy;
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int Max_Num = INT_MIN, n = candies.size();
+        for(int Candy : candies){
+            if(Candy>Max_Num){
+                Max_Num = Candy;
             }
         }
-
-        for(int i=0 ; i<n ; i++){
-            if(Candies[i]+ ExtraCandies >= Max_Candies){
+        vector<bool>Result(n,false);
+        for(int i=0; i<n ; i++){
+            if( (candies[i] + extraCandies) >=Max_Num){
                 Result[i]=true;
             }
         }
         return Result;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* int MaxVal_Vector(vector<int> V){
-        int ResultMax=V[0];
-        for(int i = 1 ; i < V.size() ; i++){
-            if (V[i] > ResultMax){
-                ResultMax = V[i];
-            }
-        }
-        return ResultMax;
-    }
-    */
-
-      // vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-       
-   
-
-    /* vector<bool>Result(candies.size(),false);
-
-        int MaxElement= MaxVal_Vector(candies);
-
-        for(int j= 0 ; j <candies.size() ; j++){
-            if(candies[j] + extraCandies >= MaxElement){
-                Result[j] = true;
-            }
-        }
-      
-        for(int i=0 ; i<candies.size();i++){
-              int MaxVal=0;
-            for(int j=0; j<candies.size();j++){
-                if(candies[j]>MaxVal & j!=i){
-                    MaxVal = candies[j];
-                }
-            }
-            if(MaxVal <= candies[i] + extraCandies){
-                Result[i]=true;
-            }
-        }
-        //return Result;
-       
-    }
-    */
