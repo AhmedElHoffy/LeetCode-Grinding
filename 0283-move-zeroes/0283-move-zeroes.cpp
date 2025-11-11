5d1b9cdaa3c564aae313818372065ca0caa28dd5
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-11-11 00:13:13 -0800

    Time: 0 ms (100%), Space: 23.8 MB (55.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,79 +1,19 @@
 class Solution {
-private: 
-    void Swap_Nums(int &a, int &b){
-        int temp=a;
+public:
+    void swap_nums(int &a, int &b){
+        int temp = a;
         a = b;
         b = temp;
     }
-
-public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int Write_Idx=0;
-        for(int i=0; i < n ; i++){
+        int NonZero_idx=0;
+
+        for(int i=0 ; i<n ; i++){
             if(nums[i]!=0){
-                nums[Write_Idx++] = nums[i];
+                nums[NonZero_idx++]=nums[i];
             }
         }
-        for(int j=Write_Idx ; j < n ; j++){
-            nums[j]=0;
-        }
+        while(NonZero_idx < n) nums[NonZero_idx++]=0;   
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-class Solution {
-public:
-
-void swap_nums(int &x, int &y){
-    int temp = x;
-    x = y;
-    y = temp;
-
-}
-    void moveZeroes(vector<int>& nums) {
-        int lastNonZeroFoundAt = 0; // Pointer for the position of the next non-zero element
-
-        // Move all non-zero elements to the beginning of the array
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[lastNonZeroFoundAt++] = nums[i];
-            }
-        }
-
-        // Fill the rest of the array with zeroes
-        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
-            nums[i] = 0;
-        }
-    }
-    
-};
-*/
