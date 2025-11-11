2a5d5d6e3d25348c1288ec8a405bd750a4f4c2d6
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-11-11 00:00:47 -0800

    Time: 0 ms (100.00%) | Memory: 40.3 MB (44.07%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
+        vector<int>Res(n,1);
+
+        int Prefix = 1;
+        for(int i=0; i<n ; i++){
+            Res[i]=Prefix;
+            Prefix*=nums[i];
+        }
+
+        int Postfix = 1;
+        for(int j=n-1; j>=0 ; j--){
+            Res[j]*=Postfix;
+            Postfix*=nums[j];
+        }
+        return Res;
+    }
+};
+
+
+/*
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
         vector<int>PreFix_Prod(n,1),PostFix_Prod(n,1);
         int Curr_Total=1;
         for(int i=0 ; i<n ; i++){
@@ -20,4 +41,4 @@ public:
         }
         return Result;
     }
-};
+*/
