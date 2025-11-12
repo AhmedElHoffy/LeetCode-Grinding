d4857b5339bcca740ba12f331e969ad77cb3b7a3
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-11-12 08:26:40 -0800

    Time: 2 ms (54.02%) | Memory: 7.8 MB (65.03%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,55 +1,21 @@
 class Solution {
-public: 
+public:
     int minFlips(int a, int b, int c) {
-        int count = 0;
-      
-      
-      // Bit Manipulation Solution
-      /*
-      while( a>0 || b>0 || c>0){
-        //(LSB extract)
-        int bit_a = a & 1; 
-        int bit_b = b & 1;
-        int bit_c = c & 1;
+        int Total_Flips=0;
+        int Curr_bitA, Curr_bitB, Curr_bitC;
+        for (int bit =0 ; bit <32 ; bit++){
+            Curr_bitA = (a >> bit) & 1;
+            Curr_bitB = (b >> bit) & 1;
+            Curr_bitC = (c >> bit) & 1;
 
-        // if LSB at c is zero , LSB at A and at B are 1's , we need to flip both of them
-        if(bit_c==0){
-            count+= bit_a + bit_b;
-        }else{
-            // bit_c == 1 , we flip only 1 of A or B if botha are 0s 
-            if(bit_a==0 && bit_b==0){
-                count++;
-            }
+            if(Curr_bitC==1){
+                if(Curr_bitA==0 && Curr_bitB==0){
+                    Total_Flips++;
+                }
+            }else{
+                Total_Flips += Curr_bitA + Curr_bitB;
+            } 
         }
-        //SHR
-        a >>= 1;
-        b >>= 1;
-        c >>= 1;
-      }
-      */
-
-      // Calculating the Remainder solution (Slower)
-
-       while( a>0 || b>0 || c>0){
-        //(LSB extract)
-        int bit_a = a % 2; 
-        int bit_b = b % 2;
-        int bit_c = c % 2;
-
-        // if LSB at c is zero , LSB at A and at B are 1's , we need to flip both of them
-        if(bit_c==0){
-            count+= bit_a + bit_b;
-        }else{
-            // bit_c == 1 , we flip only 1 of A or B if botha are 0s 
-            if(bit_a==0 && bit_b==0){
-                count++;
-            }
-        }
-        //SHR
-        a /= 2;
-        b /= 2;
-        c /= 2;
-      }
-      return count;
+        return Total_Flips;
     }
 };
