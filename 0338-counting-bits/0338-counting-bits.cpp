4d17f3aa9b96001741e9535b265c72a6ab07f935
Ahmed ElHoffy <108846700+AhmedElHoffy@users.noreply.github.com>
2025-11-12 08:14:07 -0800

    Time: 0 ms (100%), Space: 10.5 MB (97.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,23 +1,10 @@
 class Solution {
-
-private:
-    int Num2Bin_Conv(int x){
-        int CountOnes=0;
-        while(x>0){
-            CountOnes+=(x%2);
-            x/=2;
-        }
-        return CountOnes;
-    }
-
 public:
     vector<int> countBits(int n) {
-        vector<int>Ans(n+1,0);
-
-        for(int i=0 ; i <=n ; i++){
-            Ans[i] = Num2Bin_Conv(i);
-        }
-
-      return Ans;  
+       vector<int>ans(n+1,0);
+       for(int i=1 ; i<=n ; i++){
+        ans[i] = ans[i >> 1] + (i & 1);
+       }
+       return ans;
     }
 };
